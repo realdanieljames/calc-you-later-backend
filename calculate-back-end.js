7c8b06537046bc622518a12dbb0aa9ff2359c325
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-06-22 18:00:07 -0400

    completed calculation tasks, also updated code from an if-statement to a switch-statement, to help it look cleaner and more readable

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -2,8 +2,57 @@
  * YOUR CODE HERE *
  ******************/
 
+// function calculate(num1, num2, operator){
+//   if (operator === '+' || operator === 'plus' || operator === 'added to'){
+//     return parseInt(num1) + parseInt(num2);
+//   }
+//   if (operator === '-' || operator === 'minus' || operator === 'subtracted from'){
+//     return parseInt(num1) - parseInt(num2);
+//   }
+//   if (operator === 'x' || operator === 'times' || operator === 'X' || operator === 'multiplied by' ){
+//     return parseInt(num1) * parseInt(num2);
+//   }
+//   if (operator === '/' || operator === 'divided by'){
+//     return parseInt(num1) / parseInt(num2);
+//   }
+//   if (operator === '%' || operator === 'modulus' || operator === 'mod'){
+//     return parseInt(num1) % parseInt(num2);
+//   }
+//   else{return "Sorry, that's not a mathematical operation!"}
+// }
 
- 
+
+function calculate(num1, num2, operator){
+  switch(operator) {
+    case '+': 
+    case 'plus': 
+    case 'added to':
+    return parseInt(num1) + parseInt(num2);
+    
+    case "-": 
+    case "minus": 
+    case "subtracted from":
+    return parseInt(num1) - parseInt(num2);
+
+    case 'x': 
+    case 'times': 
+    case 'X': 
+    case "multiplied by":
+    return parseInt(num1) * parseInt(num2);
+
+    case '/': 
+    case 'divided by': 
+    return parseInt(num1) / parseInt(num2);
+
+    case '%': 
+    case 'modulus': 
+    case "mod": 
+    return parseInt(num1) % parseInt(num2);
+      
+    default:
+    return "Sorry, that's not a mathematical operation!"
+
+  }}
 
 /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
